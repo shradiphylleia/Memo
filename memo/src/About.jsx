e54b0e90b7a94c735262e75868c7019b4270ca30
shradiphylleia <shradulsha@gmail.com>
2024-10-20 23:03:28 +0530

    about page and the error page

--- memo/src/About.jsx
+++ memo/src/About.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Container from './Container.jsx'
+import Button from './Button.jsx'
+import './About.css'
 
 function About() {
   return (
-    <div>About</div>
+   <>
+   <div className="aboutDiv">
+   <h1>Premise :)</h1>
+   <p>We attend alot of events but miss out on the event photographs and overall hightlights.
+    As event organizers share canvas link with the attended who can add the highlights from their end.
+    Allow the attendees to share event highlights.
+    Additionaly the users can maintain and manage the contacts they have made during the sttended events.</p>
+
+    <Container className='cntrAbout'>
+      <Button className='signupAbout' link='/signup' text='try now'></Button>
+    </Container>
+
+    <div className="creator">
+    <h4>Dev team:shraddha/shradiphylleia</h4>
+    <p>The creator is a student based out of India. She likes community events and has a nick for photography. In her solitary moments she can be seen enjoying long form content around critical analysis of cinematograhy among others.</p>
+    </div>
+    </div>
+   </>
   )
 }
 
--- memo/src/NotFound.jsx
+++ memo/src/NotFound.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './NotFound.css'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <>
+    <div className='mainDiv'>
+      <h1>404 - Page Not Found</h1>
+      <p>Uh-oh! It seems the page you're looking for doesn't exist or has been moved.</p>
+      <p>Don't worry, you can head back to the homepage or check the URL and try again.</p>
+      <a href="/">Back to Home</a>
+    </div>
+    </>
+  );
 }
 
 export default NotFound
