4e503c1ec1338c97596234c3eec242d9d64b0382
shradiphylleia <shradulsha@gmail.com>
2024-10-30 16:56:34 +0530

    card component updated

--- memo/src/Card.jsx
+++ memo/src/Card.jsx
@@ -27,22 +27,21 @@ const [position, setPositon] = useState(JSON.parse(cardData.position));
 
 
 const mouseMove = (e) => {
-    //1 - Calculate move direction
     let mouseMoveDir = {
         x: mouseStartPos.x - e.clientX,
         y: mouseStartPos.y - e.clientY,
     };
  
-    //2 - Update start position for next move.
     mouseStartPos.x = e.clientX;
     mouseStartPos.y = e.clientY;
- 
-    //3 - Update card top and left position.
+
     setPositon({
         x: cardRef.current.offsetLeft - mouseMoveDir.x,
         y: cardRef.current.offsetTop - mouseMoveDir.y,
     });
 };
+
+
 
 const mouseUp = () => {
     document.removeEventListener("mousemove", mouseMove);
